77f7c5c48662e6b787abab401daa4560439292cb
cabaelamirxle <amirxlecabael28@gmail.com>
2022-12-16 18:42:20 +0800

    Queue with Priority

---
+++ queue-with-priority.py
@@ -0,0 +1,11 @@
+from heapq import heappop, heappush
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
